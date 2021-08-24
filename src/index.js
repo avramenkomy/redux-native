c3771875171a5a9f287f4992db77f63e7c3cd1a8
avramenkomy <avramenkomy@yandex.ru>
2021-08-24 14:53:13 +0300

    WIP: add reducer and redux install

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import {createStore} from "./createStore";
+import {createStore} from "redux";
+import {rootReducer} from "./redux/rootReducer";
 import './styles.css';
 
 // поиск элементов в DOM-дереве по id
@@ -10,15 +11,16 @@ const themeBtn = document.getElementById('theme');
 
 // создаем объект store, который умеет взаимодействовать с данными и сообщать компоненту,
 // что произошли те или иные изменения
-const store = createStore();
+// при создании передаем reducer без вызова как референс и начальное состояние счетчика
+const store = createStore(rootReducer, 0);
 
 // добавляем прослушку событий
 addBtn.addEventListener('click', () => {
-
+    store.dispatch({ type: 'INCREMENT' });
 });
 
 subBtn.addEventListener('click', () => {
-
+    store.dispatch({ type: 'DECREMENT' });
 });
 
 asyncBtn.addEventListener('click', () => {
@@ -28,3 +30,11 @@ asyncBtn.addEventListener('click', () => {
 themeBtn.addEventListener('click', () => {
 
 });
+// подписываемся на изменения store
+store.subscribe(() => {
+    const state = store.getState(); // получаем state
+    counter.textContent = state; // передаем значение в DOM ноду counter  и перерисовываем компонент
+});
+
+// что бы счетчие отрисовывался из начального состояния приложения нужно задиспатчить несущетсвующий экшн
+store.dispatch({ type: 'INIT_APP' });
